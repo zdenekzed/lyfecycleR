d0c43286fd691de2130cc5d8c0cf13b6555c4050
zdenekzed <zhejnak@gmail.com>
2018-07-23 16:37:57 +0200

    Update lyfecycle.R
    uprava ..

--- lyfecycle.R
+++ lyfecycle.R
@@ -1,9 +1,13 @@
 # reporting date
 today <- as.Date('2012-04-11', format='%Y-%m-%d')
  
-# processing data
+# processing data - napojime na data z vytvoreneho datasetu
 orders <- dcast(orders, orderId + clientId + gender + orderdate ~ product, value.var='product', fun.aggregate=length)
  
+# Therefore, we need to calculate two values:
+# 1/ number of orders that were placed by each client (or in some cases, it can be the number of items) - F,
+# 2/ time lapse from the last purchase to the reporting date - R.
+
 orders <- orders %>%
  group_by(clientId) %>%
  mutate(frequency=n(),
@@ -12,20 +16,10 @@ orders <- orders %>%
  filter(orderId==max(orderId)) %>%
  ungroup()
  
-# exploratory analysis - TOHLŮE NEDELANE
-ggplot(orders, aes(x=frequency)) +
- theme_bw() +
- scale_x_continuous(breaks=c(1:10)) +
- geom_bar(alpha=0.6, binwidth=1) +
- ggtitle("Dustribution by frequency")
- 
-ggplot(orders, aes(x=recency)) +
- theme_bw() +
- geom_bar(alpha=0.6, binwidth=1) +
- ggtitle("Dustribution by recency")
-
 ############################
-# TED DALSI KROK - PRIADME SEGMENTY
+# We will use the following boundaries:
+# for frequency: 1, 2, 3, 4, 5, >5,
+# for recency: 0-6, 7-13, 14-19,  20-45, 46-80, >80
 
 orders.segm <- orders %>%
  mutate(segm.freq=ifelse(between(frequency, 1, 1), '1',
@@ -49,7 +43,8 @@ orders.segm$segm.freq <- factor(orders.segm$segm.freq, levels=c('>5', '5', '4', 
 orders.segm$segm.rec <- factor(orders.segm$segm.rec, levels=c('>80 days', '46-80 days', '20-45 days', '14-19 days', '7-13 days', '0-6 days'))
 
 ##############
-# We have everything need to create LifeCycle Grids. We need to combine clients into segments with the 
+# We have everything need to create LifeCycle Grids. 
+# We need to combine clients into segments 
 ##############
 lcg <- orders.segm %>%
  group_by(segm.rec, segm.freq) %>%
